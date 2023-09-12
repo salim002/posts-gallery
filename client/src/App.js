7784168e0968b5c205bc0c22b498251cca05b961
MD SALIM <mdsalim.iiitr@gmail.com>
2023-09-12 10:05:30 +0530

    Done basic setups for frontend, added style.scss file to style pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,66 @@
+import "./style.scss"
+
+import Register from "./pages/Register";
+import Login from "./pages/Login"
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+
+const Layout = ()=>{
+  return (
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/post/:id",
+        element: <Single/>
+      },
+      {
+        path: "/write",
+        element: <Write/>
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
-        Hello World
+        <div className="container">
+          <RouterProvider router={router}/>
+        </div>
     </div>
   );
 }
