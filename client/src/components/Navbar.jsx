76514766d1b861f5b0328bb4f32ca2ee4daba62c
MD SALIM <mdsalim.iiitr@gmail.com>
2023-09-12 11:15:47 +0530

    done basic setup of Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Logo from "../img/logo.png";
 
-export default function Navbar() {
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="logo image not found" />
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className="link" to="/?cat=science">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className="link" to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link" to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <span>John</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">Write</Link>
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
+
+export default Navbar
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from "react"
+import {Link} from "react-router-dom";
 
-export default function Login() {
+const Login = () => {
   return (
     <div className="auth">
       <h1>Login</h1>
@@ -8,7 +9,12 @@ export default function Login() {
         <input type="text" placeholder="username" />
         <input type="password" placeholder="password" />
         <button>Login</button>
+        <p>This is an error!</p>
+        <span>Don't you have an account? <Link to="/register" >Register</Link> </span>
       </form>
     </div>
   )
 }
+
+export default Login
+
