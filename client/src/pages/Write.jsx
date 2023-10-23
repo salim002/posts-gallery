118026914951ca696612cc07ad06f409a5176243
MD SALIM <mdsalim.iiitr@gmail.com>
2023-10-23 09:22:45 +0530

    Done some modifications in design

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -64,19 +64,11 @@ const Write = () => {
         </div>
       </div>
       <div className="menu">
-        <div className="item">
-          <h1>Publish</h1>
-          <span>
-            <b>Status: </b> Draft
-          </span>
-          <span>
-            <b>Visibility: </b> Public
-          </span>
+        <div className="upload-and-post">
           <input style={{display: "none"}} type="file" id="file" name="" onChange={e=>setFile(e.target.files[0])} />
           <label className="file" htmlFor="file">Upload Image</label>
           <div className="buttons">
-            {/* <button>Save as a draft</button> */}
-            <button onClick={handleClick} style={{width: "100px"}}>Post</button>
+            <button onClick={handleClick}>Add</button>
           </div>
         </div>
         <div className="item">
